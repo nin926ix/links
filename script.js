5b63490679595520824a591834b2751454b8683c
markelite <mikhailovm123@gmail.com>
2024-01-30 14:56:39 +0100

    Init commit

---
+++ script.js
@@ -0,0 +1,44 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const firstNumber = document.getElementById('first-number');
+    const secondNumber = document.getElementById('second-number');
+    const lowerNumber = document.getElementById('lower-number');
+    const upperNumber = document.getElementById('upper-number');
+
+    let secondCounter = 21;
+    let secondInterval;
+
+    const animateSecondNumber = () => {
+        secondNumber.style.opacity = 1;
+        lowerNumber.style.opacity = 1;
+        upperNumber.style.opacity = 1;
+        secondCounter = (secondCounter + 1) % 24;
+        secondNumber.textContent = secondCounter % 12 || 12;
+        lowerNumber.textContent = (secondCounter + 11) % 12 || 12;
+        upperNumber.textContent = (secondCounter + 1) % 12 || 12;
+
+        if (secondCounter === 6) {
+            lowerNumber.style.opacity = 0;
+            upperNumber.style.opacity = 0;
+            setTimeout(restartAnimation, 4000);
+            clearInterval(secondInterval);
+        }
+    };
+
+    function restartAnimation() {
+        // Reset first number opacity
+        firstNumber.style.opacity = 0;
+        secondNumber.style.opacity = 0;
+
+        setTimeout(() => {
+            firstNumber.style.opacity = 1;
+            secondInterval = setInterval(animateSecondNumber, 600);
+        }, 2000);
+
+        secondCounter = 21;
+    }
+
+    setTimeout(() => {
+        firstNumber.style.opacity = 1;
+        secondInterval = setInterval(animateSecondNumber, 600);
+    }, 1000);
+});
